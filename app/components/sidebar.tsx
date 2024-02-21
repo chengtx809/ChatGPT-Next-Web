75e0eaa40b938522c3ecdee761da8a88ee806305
zklcdc <zklcdc@qq.com>
2024-02-21 21:04:12 +0800

    [Fix] 🎈 Sidebar SubTitle

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           BingAPI Demo
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Proudly adopting NextChat.
+          Just for test.<br>Proudly adopting NextChat.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
