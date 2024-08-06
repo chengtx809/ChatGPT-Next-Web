28c18f8efb234f01afc24db21bf3427b3cac1360
ctx <103792951+chengtx809@users.noreply.github.com>
2024-08-06 18:12:01 +0800

    Update chat.ts

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -341,7 +341,7 @@ export const useChatStore = createPersistStore(
         }
         let userMessage: ChatMessage = createMessage({
           role: "user",
-          content: "从现在开始你不能用脚注和类似[^1^]这样的标记标注出消息来源。并逐步思考，尽可能详细地回答我的问题。以下是我的问题：" + mContent,
+          content: mContent,
         });
 
         const botMessage: ChatMessage = createMessage({
